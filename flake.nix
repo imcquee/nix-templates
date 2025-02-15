23193326c53fbc044d096f32cd6766856eb131c4
Isaac McQueen <imcqueen@truehomesusa.com>
2025-02-15 14:59:08 -0500

    add back self

--- flake.nix
+++ flake.nix
@@ -1,23 +1,24 @@
 {
   description = "Nix Development Templates";
-  outputs = {
-    templates = {
-      elixir = {
-        path = ./elixir;
-        description = "Elixir development environment";
-      };
-      dotnet = {
-        path = ./dotnet;
-        description = "Dotnet development environment";
-      };
-      gleam = {
-        path = ./gleam;
-        description = "Gleam development environment";
-      };
-      typescript = {
-        path = ./typescript;
-        description = "Typescript development environment";
+  outputs = { self }:
+    {
+      templates = {
+        elixir = {
+          path = ./elixir;
+          description = "Elixir development environment";
+        };
+        dotnet = {
+          path = ./dotnet;
+          description = "Dotnet development environment";
+        };
+        gleam = {
+          path = ./gleam;
+          description = "Gleam development environment";
+        };
+        typescript = {
+          path = ./typescript;
+          description = "Typescript development environment";
+        };
       };
     };
-  };
 }
