6c207dbc5b7ea313d8f8a8f0dbb84c2a0d4f3cfb
Isaac McQueen <imcqueen@truehomesusa.com>
2025-02-17 07:11:26 -0500

    update helix conf

--- elixir/flake.nix
+++ elixir/flake.nix
@@ -1,14 +1,9 @@
 {
-  description = "Basic flake template with a dev shell and an app";
+  description = "Development environment with Helix configuration";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   outputs = { nixpkgs, ... }:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
       });
@@ -16,6 +11,22 @@
     {
       devShells = forAllSystems ({ pkgs }:
         let
+          # Create a derivation for the Helix config
+          helixConfig = pkgs.runCommand "helix-config" { } ''
+            mkdir -p $out/.helix
+            cat > $out/.helix/config.toml << EOF
+            theme = "catppuccin_mocha"
+            [editor]
+            true-color = true
+            [keys.normal]
+            A-g = ":sh zellij run -c -f -x 10% -y 10% --width 80% --height 80% -- lazygit"
+            [keys.normal.A-y]
+            y = ":sh zellij run -c -f -x 10% -y 10% --width 80% --height 80% -- nix run .#yaziPicker open"
+            v = ":sh zellij run -c -f -x 10% -y 10% --width 80% --height 80% -- nix run .#yaziPicker vsplit"
+            h = ":sh zellij run -c -f -x 10% -y 10% --width 80% --height 80% -- nix run .#yaziPicker hsplit"
+            EOF
+          '';
+
           layoutFile = pkgs.writeText "layout.kdl" ''
             pane_frames false
             default_shell "fish"
@@ -25,54 +36,40 @@
                 args "."
               }
             }
-            keybinds {
-              shared {
-                bind "Alt y" {
-                  Run "nix" "run" ".#yazizj" {
-                    in_place true
-                  }
-                }
-                bind "Alt g" { 
-                  Run "zellij" "run" "-cf" "--width" "80%" "--height" "80%" "--x" "10%" "--y" "10%" "--" "lazygit" {
-                    close_on_exit true
-                  }
-                }
-              }
-            }
           '';
         in
         {
           default = pkgs.mkShell {
-            packages =
-              with pkgs;
-              [
-                ps
-                zellij
-                yazi
-                helix
-                elixir
-                elixir-ls
-                git
-              ]
-              ++
-              pkgs.lib.optionals pkgs.stdenv.isLinux (with pkgs; [
-                inotify-tools
-              ]);
+            packages = with pkgs; [
+              elixir
+              elixir-ls
+            ] ++ lib.optionals stdenv.isLinux [ inotify-tools ];
+
             shellHook = ''
+              # Use symbolic link instead of copying
+              ln -sfn ${helixConfig}/.helix .
               if ! ps aux | grep -q '[z]ellij'; then
                 zellij -l ${layoutFile}
               fi
             '';
           };
         });
+
       apps = forAllSystems ({ pkgs }: {
-        yazizj = {
+        yaziPicker = {
           type = "app";
-          program = "${pkgs.writeScript "yazizj" ''
-                  #!/bin/sh
-                  yazi
-                  $EDITOR .
-                ''}";
+          program = toString (pkgs.writeScript "yaziPicker" ''
+            #!/usr/bin/env bash
+            paths=$(yazi --chooser-file=/dev/stdout | while read -r; do printf "%q " "$REPLY"; done)
+            if [[ -n "$paths" ]]; then
+              zellij action toggle-floating-panes
+              zellij action write 27 # send <Escape> key
+              zellij action write-chars ":$1 $paths"
+              zellij action write 13 # send <Enter> key
+            else
+              zellij action toggle-floating-panes
+            fi
+          '');
         };
       });
     };
