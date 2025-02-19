3841474c5b711e1a70e16fd8729b2bc3057060fb
Isaac McQueen <imcqueen@truehomesusa.com>
2025-02-19 15:05:51 -0500

    add typst

---
+++ typst/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Typescript development environment";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  outputs = { nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }:
+        let
+          layoutFile = pkgs.writeText "layout.kdl" ''
+            pane_frames false
+            layout {
+              pane {
+                command "hx"
+                args "."
+              }
+            }
+          '';
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              typst
+              zathura
+              pandoc
+            ];
+
+            shellHook = ''
+              if [ -z "$ZELLIJ" ] || [ "$ZELLIJ" -ne 0 ]; then
+                zellij -l ${layoutFile}
+              fi
+            '';
+          };
+        });
+    };
+}
