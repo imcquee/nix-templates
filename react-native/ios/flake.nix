7dc32b5819b48be77e4d72e7168080df2779bccc
Isaac McQueen <imcqueen@truehomesusa.com>
2025-08-02 08:15:17 -0400

    wip

---
+++ react-native/ios/flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "MacOS React-Native/Expo template";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              bun
+              eas-cli
+              typescript-language-server
+              vscode-langservers-extracted
+              cocoapods
+              sd
+              fastlane
+            ];
+
+            shellHook = ''
+              export PATH=$(echo $PATH | sd "${pkgs.xcbuild.xcrun}/bin" "")
+              unset DEVELOPER_DIR
+            '';
+          };
+        }
+      );
+    };
+}
